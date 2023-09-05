c95e87f2c6d2a9b8b02e19c5abf8d2df945f35fa
evanator05 <watsonevan2005@gmail.com>
2023-09-05 03:40:42 -0600

    some changes

--- Control3D.gd
+++ Control3D.gd
@@ -1,22 +1,47 @@
-##A node used as the parent of other control nodes to display them in 3D
+##A node used as the parent of other control nodes to display them in 3D.
 @tool class_name Control3D extends MeshInstance3D
 
-##Sets size of the [QuadMesh]
+##The size of the [QuadMesh].
 @export var size:Vector2 = Vector2(1, 1) :
 	set(value):
 		size = value
 		mesh.size = value
+		if force_viewport_aspect_ratio:
+			set_viewport_size(value*viewport_resolution_scale)
 
-##Sets the billboard mode of the [QuadMesh]
+##The resolution of the [SubViewport].
+@export var viewport_resolution:Vector2 = Vector2(512, 512):
+	set(value):
+		viewport_resolution = value
+		set_viewport_size(value)
+
+##If set to true it overwrites the [SubViewport]s size with size*viewport_resolution_scale.
+@export var force_viewport_aspect_ratio:bool = false
+##Only used if force_viewport_aspect_ratio is set to true.
+##Overwrites the [SubViewport]s size with size*viewport_resolution_scale.
+@export var viewport_resolution_scale:int = 512:
+	set(value):
+		viewport_resolution_scale = value
+		if force_viewport_aspect_ratio:
+			set_viewport_size(size*value)
+
+##Sets the billboard mode of the [QuadMesh].
 @export_enum("Disabled", "Enabled", "Y-Billboard") var billboard_mode:int = 0 :
 	set(value):
 		billboard_mode = value
 		mesh.surface_get_material(0).billboard_mode = value
 
+##Refreshes the [SubViewport]s children. Meant only for use in the editor
+@export var force_refresh_viewport:bool = false:
+	set(value):
+		refresh_viewport()
+
 func _ready() -> void:
 	var subViewport := SubViewport.new()
 	subViewport.name = "SubViewport"
 	subViewport.disable_3d = true
+	set_viewport_size(viewport_resolution)
+	if force_viewport_aspect_ratio: set_viewport_size(size*viewport_resolution_scale)
 	add_child(subViewport, true)
 	mesh = QuadMesh.new()
 	mesh.size = size
@@ -32,29 +57,26 @@ func _ready() -> void:
 			$SubViewport.add_child(child)
 
 ##Gets all of the [SubViewport]s child nodes
-##[codeblock]
-##for child in $Control3D.getNodes():
-##    child.value = 8
-##[/codeblock]
 func get_control_nodes(include_internal: bool = false) -> Array[Node]:
 	return $SubViewport.get_children(include_internal)
 
-func clearViewport():
+##Deletes all of the [SubViewport]s children
+func clear_viewport():
 	for child in get_control_nodes():
 		child.queue_free()
 
-func addControlToViewport():
-		for child in get_children():
-			if child is Control:
-				var new = child.duplicate()
-				remove_child(new)
-				$SubViewport.add_child(new)
+##Adds all [Control] node children to the [SubViewport]
+func add_control_to_viewport():
+	for child in get_children():
+		if child is Control:
+			var new = child.duplicate()
+			$SubViewport.add_child(new)
 
-func refreshViewport():
-	clearViewport()
-	addControlToViewport()
+##Sets the size of the [SubViewport]
+func set_viewport_size(s:Vector2):
+	$SubViewport.size = s
 
-func _notification(what):
-	if Engine.is_editor_hint(): return
-	if what == NOTIFICATION_CHILD_ORDER_CHANGED:
-		refreshViewport()
+##Calls clears the [SubViewport]s children then adds the new [Control] nodes
+func refresh_viewport():
+	clear_viewport()
+	add_control_to_viewport()
